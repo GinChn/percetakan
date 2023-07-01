0087d7f47f874a80a4c321977eb63ecb74bff0bc
def <wahni5588@gmail.com>
2023-07-01 11:57:51 +0800

    need to change

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -1,35 +1,62 @@
 @extends('layout')
 
 @section('content')
-
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Laporan</h1>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Laporan</h1>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="container-fluid">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="dropdownInput">Jenis Laporan</label>
+                                    <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <label class="input-group-text" for="dropdownInput">Laporan</label>
+                                        </div>
+                                        <select class="custom-select" id="dropdownInput">
+                                            <option selected>Harian</option>
+                                            <option value="option1">Bulanan</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="dateInput">Tanggal</label>
+                                    <input type="date" class="form-control" id="dateInput" placeholder="Select date">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
-        <div class="col-12">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-default">
-                        Tambah Laporan
-                    </button>
+                    <h5 class="card-title">Pemasukan</h5>
                 </div>
                 <div class="card-body">
-                    <table id="table2" class="table table-bordered table-striped">
+                    <table class="table">
                         <thead>
                             <tr>
-                                <th>No</th>
-                                <th>Data 1</th>
-                                <th>Data 1</th>
-                                <th>Data 1</th>
-                                <th>Data 1</th>
+                                <th>No Nota</th>
+                                <th>Nama Pelanggan</th>
+                                <th>Total Tagihan</th>
                                 <th>Aksi</th>
                             </tr>
                         </thead>
@@ -38,17 +65,51 @@
                                 <td>#</td>
                                 <td>#</td>
                                 <td>#</td>
+                                <td>
+                                    <a href="#" class="btn-sm btn-primary">Detail</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="total col-md-6 float-right bg-black">
+                        <div class="row">
+                            <div class="col">
+                                <p>Total Pemasukan =</p>
+                            </div>
+                            <div class="col">
+                                <p class="text-bold">123455</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">Pengeluaran</h5>
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>No Nota</th>
+                                <th>Nama Pelanggan</th>
+                                <th>Total Tagihan</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>#</td>
                                 <td>#</td>
                                 <td>#</td>
                                 <td>
-                                    <a href="#" class="btn-sm btn-primary"><i class="fas fa-pen"></i></a>
-                                    <form id="#" action="#" method="post" class="d-inline">
-                                        @csrf
-                                        @method('delete')
-                                        <button type="submit" class="btn-sm btn-danger border-0 delete-btn">
-                                            <i class="fa fa-trash"></i>
-                                        </button>
-                                    </form>
+                                    <a href="#" class="btn-sm btn-primary">Detail</a>
                                 </td>
                             </tr>
                         </tbody>
@@ -57,6 +118,37 @@
             </div>
         </div>
     </div>
-</div>
 
-@endsection
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-body">
+                            <table id="table2" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>No Nota</th>
+                                        <th>Nama Pelanggan</th>
+                                        <th>Total Tagihan</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>#</td>
+                                        <td>#</td>
+                                        <td>
+                                            <a href="#" class="btn-sm btn-primary">Detail</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
