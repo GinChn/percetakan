cb125d45601618b7d771b1a7fe61bc57bbbfa1bf
def <wahni5588@gmail.com>
2023-07-25 01:08:22 +0800

    checkbox di samping

--- resources/views/pekerjaan/belum_ada_desain.blade.php
+++ resources/views/pekerjaan/belum_ada_desain.blade.php
@@ -1,6 +1,93 @@
 @extends('layout')
 
 @section('content')
+    <style>
+        .checkbox-wrapper-46 input[type="checkbox"] {
+            display: none;
+            visibility: hidden;
+        }
+
+        .checkbox-wrapper-46 .cbx {
+            margin: auto;
+            -webkit-user-select: none;
+            user-select: none;
+            cursor: pointer;
+        }
+
+        .checkbox-wrapper-46 .cbx span {
+            display: inline-block;
+            vertical-align: middle;
+            transform: translate3d(0, 0, 0);
+        }
+
+        .checkbox-wrapper-46 .cbx span:first-child {
+            position: relative;
+            width: 18px;
+            height: 18px;
+            border-radius: 3px;
+            transform: scale(1);
+            vertical-align: middle;
+            border: 1px solid #9098A9;
+            transition: all 0.2s ease;
+        }
+
+        .checkbox-wrapper-46 .cbx span:first-child svg {
+            position: absolute;
+            top: 3px;
+            left: 2px;
+            fill: none;
+            stroke: #FFFFFF;
+            stroke-width: 2;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: 16px;
+            stroke-dashoffset: 16px;
+            transition: all 0.3s ease;
+            transition-delay: 0.1s;
+            transform: translate3d(0, 0, 0);
+        }
+
+        .checkbox-wrapper-46 .cbx span:first-child:before {
+            content: "";
+            width: 100%;
+            height: 100%;
+            background: #28a745;
+            display: block;
+            transform: scale(0);
+            opacity: 1;
+            border-radius: 50%;
+        }
+
+        .checkbox-wrapper-46 .cbx span:last-child {
+            padding-left: 8px;
+        }
+
+        .checkbox-wrapper-46 .cbx:hover span:first-child {
+            border-color: #28a745;
+        }
+
+        .checkbox-wrapper-46 .inp-cbx:checked+.cbx span:first-child {
+            background: #28a745;
+            border-color: #28a745;
+            animation: wave-46 0.4s ease;
+        }
+
+        .checkbox-wrapper-46 .inp-cbx:checked+.cbx span:first-child svg {
+            stroke-dashoffset: 0;
+        }
+
+        .checkbox-wrapper-46 .inp-cbx:checked+.cbx span:first-child:before {
+            transform: scale(3.5);
+            opacity: 0;
+            transition: all 0.6s ease;
+        }
+
+        @keyframes wave-46 {
+            50% {
+                transform: scale(0.9);
+            }
+        }
+    </style>
     <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
@@ -17,7 +104,7 @@
                 <div class="card">
 
                     <div class="card-body">
-                        <table id="table2" class="table table-bordered table-striped">
+                        <table class="table table-bordered table-striped">
                             <thead>
                                 <tr>
                                     <th width="5%">No</th>
@@ -30,29 +117,42 @@
                                     <th>L</th>
                                     <th>QTY</th>
                                     <th>Finishing</th>
-
-                                    <th>Aksi</th>
+                                    {{-- Add a new table header for checkboxes --}}
+                                    <th>Select</th>
+                                    {{-- 
+                                    <th>Aksi</th> --}}
                                 </tr>
                             </thead>
-                            <tbody></tbody>
-                            @foreach ($data as $item)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{ $item->no_nota }}</td>
-                                    <td>{{ tanggal_indonesia($item->created_at) }}</td>
-                                    <td>{{ $item->nama_pelanggan }}</td>
-                                    <td>{{ $item->nama_pesanan }}</td>
-                                    <td>{{ $item->nama_barang }}</td>
-                                    <td>{{ $item->panjang }}</td>
-                                    <td>{{ $item->lebar }}</td>
-                                    <td>{{ $item->jumlah }}</td>
-                                    <td>{{ $item->nama_finishing }}</td>
-                                    <td>
-                                        <a href="#" class="btn-sm btn-success">Selesaikan</a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                            <tbody>
+                                @foreach ($data as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->no_nota }}</td>
+                                        <td>{{ tanggal_indonesia($item->created_at) }}</td>
+                                        <td>{{ $item->nama_pelanggan }}</td>
+                                        <td>{{ $item->nama_pesanan }}</td>
+                                        <td>{{ $item->nama_barang }}</td>
+                                        <td>{{ $item->panjang }}</td>
+                                        <td>{{ $item->lebar }}</td>
+                                        <td>{{ $item->jumlah }}</td>
+                                        <td>{{ $item->nama_finishing }}</td>
+                                        {{-- Add a checkbox for each row --}}
+                                        <td>
+                                            <div class="checkbox-wrapper-46">
+                                                <input class="inp-cbx" id="cbx-46" type="checkbox" />
+                                                <label class="cbx" for="cbx-46"><span>
+                                                        <svg width="12px" height="10px" viewbox="0 0 12 10">
+                                                            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+                                                        </svg></span><span></span>
+                                                </label>
+                                            </div>
 
+                                        </td>
+                                        {{-- <td>
+                                            <a href="#" class="btn-sm btn-success">Selesaikan</a>
+                                        </td> --}}
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
